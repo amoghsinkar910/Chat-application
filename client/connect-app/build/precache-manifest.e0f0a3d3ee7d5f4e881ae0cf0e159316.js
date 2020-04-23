9b8272ebffe41737c3f2d65fa96b468368ad7825
AMOGH <amogh@amogh.com>
2020-04-23 17:21:54 +0530

    First commit

---
+++ client/connect-app/build/precache-manifest.e0f0a3d3ee7d5f4e881ae0cf0e159316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be0aec14e8699cbc779866d36610ac1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "115e5f2dbf160f63a2f0",
+    "url": "/static/css/main.bb4c83c4.chunk.css"
+  },
+  {
+    "revision": "8c77033fb2aa6f79b55d",
+    "url": "/static/js/2.fa9d0c68.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.fa9d0c68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "115e5f2dbf160f63a2f0",
+    "url": "/static/js/main.3462655f.chunk.js"
+  },
+  {
+    "revision": "128d3a7bb0c8375df0df",
+    "url": "/static/js/runtime-main.2b0e221f.js"
+  }
+]);
